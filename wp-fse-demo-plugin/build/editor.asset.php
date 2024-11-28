5afcf52c993bf6fa31d41a7ea5877385efbecfba
Ethan Brackstone <dev.brackstone@gmail.com>
2024-11-28 16:28:46 +0000

    hello

--- wp-fse-demo-plugin/build/editor.asset.php
+++ wp-fse-demo-plugin/build/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '808b0583b799c6c753f0');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'd98f86454f7305a294d4');
